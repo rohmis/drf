14ef789bc34045f5813aca2f64a0165a0477885c
rohmis <romishra@bestpeers.in>
2023-11-23 15:03:40 +0530

    applied serializer

--- product/views.py
+++ product/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from .models import Blog
+from .serializer import BlogSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
+# def blog_list(request):
+    
+#     blogs=Blog.objects.all()
+    
+#     data={
+#         "Blogs": list(blogs.values())
+#     }
+#     return JsonResponse(data)
+
+# def blog_detail(request,pk):
+    
+#     blogs=Blog.objects.get(pk=pk)
+    
+#     data={
+#        'name':blogs.name,
+#        'description':blogs.description,
+#        'slug':blogs.slug,
+#     }
+#     return JsonResponse(data)
+
+@api_view(['GET'])
 def blog_list(request):
-    data={
-        "message":'hello world'
-    }
-    return JsonResponse(data)
+     all_blogs= Blog.objects.filter(is_public=True)
+     serializer= BlogSerializer(all_blogs ,many=True)
+     return Response(serializer.data)
+ 
+@api_view(['GET'])
+def blog_detail(request, pk):
+     blogs= Blog.objects.get(is_public=True, pk=pk)
+     serializer= BlogSerializer(blogs)
+     return Response(serializer.data)
