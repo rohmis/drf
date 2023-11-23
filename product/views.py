a225b5ca5aa92ca2deeacf0938e87dd6e4eaea58
rohmis <romishra@bestpeers.in>
2023-11-23 12:02:19 +0530

    print hello world through the use of jsonresponse

--- product/views.py
+++ product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def blog_list(request):
+    data={
+        "message":'hello world'
+    }
+    return JsonResponse(data)
