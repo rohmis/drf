b0b70773905e2af59305ad876cccac5a40ab3a6f
rohmis <romishra@bestpeers.in>
2023-11-27 15:38:59 +0530

    used lookup field  for params in url

--- product/views.py
+++ product/views.py
@@ -79,6 +79,7 @@ class BlogListGenericView(mixins.ListModelMixin, mixins.CreateModelMixin,generic
 class BlogDetailGenericView(mixins.RetrieveModelMixin,mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset=Blog.objects.all()
     serializer_class= BlogSerializer
+    lookup_field='slug'
     
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
