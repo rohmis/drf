f449f527a2d4486dc8bb35e74ff8dfb9db3bbf8c
rohmis <romishra@bestpeers.in>
2023-11-27 15:09:45 +0530

    retrive model mixin

--- product/views.py
+++ product/views.py
@@ -39,19 +39,7 @@ class BlogListView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-# -------------------------generic views-----------------------------------------
 
-class BlogListGenericView(mixins.ListModelMixin, mixins.CreateModelMixin,generics.GenericAPIView):
-    queryset=Blog.objects.all()
-    serializer_class= BlogSerializer
-    
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-        
 # get,put,delete
 
 class BlogDetailView(APIView):
@@ -75,3 +63,23 @@ class BlogDetailView(APIView):
           blog.delete()
           return Response(status=status.HTTP_200_OK)
         
+        
+# -------------------------generic views-----------------------------------------
+
+class BlogListGenericView(mixins.ListModelMixin, mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset=Blog.objects.all()
+    serializer_class= BlogSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+class BlogDetailGenericView(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset=Blog.objects.all()
+    serializer_class= BlogSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
