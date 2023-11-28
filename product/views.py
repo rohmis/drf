8668c5ae2e7198effa694e209a6556c550715de7
rohmis <romishra@bestpeers.in>
2023-11-28 12:50:05 +0530

    used modelViewsets through this perform crud operations

--- product/views.py
+++ product/views.py
@@ -11,50 +11,48 @@ from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
-class BlogViewSet(viewsets.ViewSet):
-    
-    # def list(self, request):
-    #     queryset = Blog.objects.filter(is_public=True)
-    #     serializer = BlogSerializer(queryset, many=True)
-    #     return Response(serializer.data)
-    
-    # def retrieve(self, request, pk=None):
-    #     queryset = Blog.objects.filter(is_public=True)
-    #     blog_list = get_list_or_404(queryset, pk=pk)
-    #     serializer = BlogSerializer(blog_list)
-    #     return Response(serializer.data)
-    
-    def list(self, request):
-        queryset = Blog.objects.filter(is_public=True)
-        serializer = BlogSerializer(queryset, many=True)
-        return Response(serializer.data)
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset=Blog.objects.filter(is_public=True)
+    serializer_class=BlogSerializer
 
-    def retrieve(self, request, pk=None):
-        queryset = Blog.objects.filter(is_public=True)
-        blog_list = get_object_or_404(queryset, pk=pk)
-        serializer = BlogSerializer(blog_list)
-        return Response(serializer.data)
+
+
+
+
+# class BlogViewSet(viewsets.ViewSet):
+
+    
+#     def list(self, request):
+#         queryset = Blog.objects.filter(is_public=True)
+#         serializer = BlogSerializer(queryset, many=True)
+#         return Response(serializer.data)
+
+#     def retrieve(self, request, pk=None):
+#         queryset = Blog.objects.filter(is_public=True)
+#         blog_list = get_object_or_404(queryset, pk=pk)
+#         serializer = BlogSerializer(blog_list)
+#         return Response(serializer.data)
     
     
-    def create(self, request):
-        serializer= BlogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def create(self, request):
+#         serializer= BlogSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def update(self, request, pk=None):
-        blog=get_object_or_404(Blog, pk=pk)
-        serializer= BlogSerializer(blog, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def update(self, request, pk=None):
+#         blog=get_object_or_404(Blog, pk=pk)
+#         serializer= BlogSerializer(blog, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def destroy(self, request, pk=None):
-        blog = get_object_or_404(Blog,pk=pk)
-        blog.delete()
-        return Response({"Message": "your blog is deleted"}, status=status.HTTP_204_NO_CONTENT)
+#     def destroy(self, request, pk=None):
+#         blog = get_object_or_404(Blog,pk=pk)
+#         blog.delete()
+        # return Response({"Message": "your blog is deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 
 # class CategoryListView(APIView):
