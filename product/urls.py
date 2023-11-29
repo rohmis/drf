16c9879d7fc16ce029d5732081d8c12c37240b6c
rohmis <romishra@bestpeers.in>
2023-11-29 12:59:32 +0530

    created category views

--- product/urls.py
+++ product/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('blog_list/', views.BlogListCreateView.as_view(),name="blog_list"),
     path("blog_detail/<int:pk>/",views.BlogDetailView.as_view(),name="blog_detail"),
+    
+    path('category_list/',views.CategoryListCreateView.as_view(),name="category_list"),
+    path('category_detail/<int:pk>/',views.CategoryDetailView.as_view(),name="category_detail"),
 
 ]
 
--- product/views.py
+++ product/views.py
@@ -1,15 +1,38 @@
 from .models import Blog,Category
 from .serializer import BlogSerializer,CategorySerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework import mixins,generics
-from rest_framework import viewsets
+from rest_framework import generics
 
-from django.shortcuts import get_object_or_404
 
 # Create your views here.
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
+    
+    def list(self, request, *args, **kwargs):
+        queryset=self.get_queryset()
+        serializer=CategorySerializer(queryset, many=True, context={'request':request})
+        if queryset.exists():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({'message':'No Category Found'},status=status.HTTP_204_NO_CONTENT)
+        
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
+    
+    def retrieve(self, request, *args, **kwargs):
+            instance=self.get_object()
+            serializer= self.get_serializer(instance)
+            
+            if instance:
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response({'Message':'No Blog Found'}, status=status.HTTP_404_NOT_FOUND)
+            
+
+
 
 class BlogListCreateView(generics.ListCreateAPIView):
     queryset=Blog.objects.filter(is_public=True)
