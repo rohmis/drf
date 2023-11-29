698f469039702f64cdc920a86936e0ba7851a456
rohmis <romishra@bestpeers.in>
2023-11-29 17:25:38 +0530

    Build a Restful api : user authentication and login/logout functionality

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+@api_view(['POST',])
+def logout_user(request):
+    if request.method == "POST":
+        request.user.auth_token.delete()
+        return Response({"message":"you are loged out"}, status=status.HTTP_200_OK)
--- django_api/urls.py
+++ django_api/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/blog/', include('product.urls')),
+    path('api/account/',include('account.urls')),
     # path('api-auth/', include('rest_framework.urls')),
 ]
